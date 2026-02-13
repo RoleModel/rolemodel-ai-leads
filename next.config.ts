c99f4d5a82f25e6d711eed511b35ca7da15f0ab8
Ben Eiwnechter <ben.einwechter@rolemodelsoftware.com>
2026-02-13 15:10:01 -0500

    Enable source map uploads

--- next.config.ts
+++ next.config.ts
@@ -77,25 +77,19 @@ const nextConfig: NextConfig = {
 }
 
 export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: 'rolemodel-software',
-
   project: 'rolemodel-ai-leads',
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+  // Show source map upload logs during build
+  silent: false,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Auth token is needed for uploading source maps
+  authToken: process.env.SENTRY_AUTH_TOKEN,
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: '/monitoring',
 })
