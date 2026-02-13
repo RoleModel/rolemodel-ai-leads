9dd9e42311674e0f16a7da1090fadcefbacfc4b9
Ben Eiwnechter <ben.einwechter@rolemodelsoftware.com>
2026-02-13 12:14:20 -0500

    Remove unneeded webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import { withSentryConfig } from '@sentry/nextjs';
+import { withSentryConfig } from '@sentry/nextjs'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
@@ -80,9 +80,9 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "rolemodel-software",
+  org: 'rolemodel-software',
 
-  project: "rolemodel-ai-leads",
+  project: 'rolemodel-ai-leads',
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
@@ -97,19 +97,5 @@ export default withSentryConfig(nextConfig, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      removeDebugLogging: true,
-    },
-  },
-});
+  tunnelRoute: '/monitoring',
+})
